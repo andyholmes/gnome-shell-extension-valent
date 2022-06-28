491d2fdeb8ade25bea0e77dcd3a63bad59009e30
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-06-28 16:21:14 -0700

    build(fix): apply suggestions from ShellCheck

--- build-aux/misc/ego.sh
+++ build-aux/misc/ego.sh
@@ -4,7 +4,7 @@
 # SPDX-FileCopyrightText: No rights reserved
 
 if [ -z "${DESTDIR}" ]; then
-    export DESTDIR=$(mktemp -d)
+    DESTDIR=$(mktemp -d) && export DESTDIR
 fi
 
 if [ -z "${WORKSPACE}" ]; then
@@ -12,7 +12,7 @@ if [ -z "${WORKSPACE}" ]; then
 fi
 
 
-BUILDDIR=${WORKSPACE}/_ego
+BUILDDIR="${WORKSPACE}"/_ego
 UUID="valent@andyholmes.ca"
 ZIPDIR="${DESTDIR}/${UUID}"
 
@@ -20,18 +20,18 @@ ZIPDIR="${DESTDIR}/${UUID}"
 # Setup
 meson setup --prefix=/usr \
             --libdir=lib \
-            ${BUILDDIR}
-meson install -C ${BUILDDIR}
+            "${BUILDDIR}"
+meson install -C "${BUILDDIR}"
 
 # Copy the installed files into a zippable directory
-mkdir -p ${ZIPDIR}/plugin
-cp -pr ${DESTDIR}/usr/lib/valent/plugins/* ${ZIPDIR}/plugin/
-cp -pr ${DESTDIR}/usr/share/gnome-shell/extensions/${UUID}/* ${ZIPDIR}
-cp -pr ${DESTDIR}/usr/share/locale ${ZIPDIR}
+mkdir -p "${ZIPDIR}"/plugin
+cp -pr "${DESTDIR}"/usr/lib/valent/plugins/* "${ZIPDIR}/plugin/"
+cp -pr "${DESTDIR}"/usr/share/gnome-shell/extensions/"${UUID}"/* "${ZIPDIR}"
+cp -pr "${DESTDIR}"/usr/share/locale "${ZIPDIR}"
 
 # Pack the directory contents into a distributable Zip
-cd ${ZIPDIR}
-zip -qr ${WORKSPACE}/${UUID}.zip .
+cd "${ZIPDIR}"
+zip -qr "${WORKSPACE}/${UUID}.zip" .
 
 echo "Extension saved to ${WORKSPACE}/${UUID}.zip"
 
