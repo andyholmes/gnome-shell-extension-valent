570e10ef401e3f9f87e2402217186f54de0fc2de
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-09-06 13:50:05 -0700

    refactor: move all quick settings UI to `status.js`
    Move all of the user interface from `extension.js` and `device.js` into
    `status.js`. This reduces the exported classes to just the indicator
    and keeps everything nice and contained.
    
    Additionally, fix several use-after-free mistakes and synchronization
    issues with the UI.

--- src/extension/extension.js
+++ src/extension/extension.js
@@ -3,30 +3,21 @@
 
 /* exported init, enable, disable */
 
-const { GLib, GObject } = imports.gi;
-
-const Main = imports.ui.main;
-const PopupMenu = imports.ui.popupMenu;
-const QuickSettings = imports.ui.quickSettings;
-const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
+const { GLib } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Extension = ExtensionUtils.getCurrentExtension();
 
 const Clipboard = Extension.imports.clipboard;
-const Device = Extension.imports.device;
 const Notification = Extension.imports.notification;
-const Remote = Extension.imports.remote;
-
-const _ = ExtensionUtils.gettext;
-const ngettext = ExtensionUtils.ngettext;
+const Status = Extension.imports.status;
 
 
 /**
- * If installed as a user extension, copy the bundled Python plugin to the
- * user plugins directory for Valent.
+ * Install the Python plugin used by Valent to communicate with GNOME Shell.
  *
- * TODO: avoid copying unchanged files
+ * This function does nothing if the extension is installed as a system
+ * extension.
  */
 function installPlugin() {
     const datadir = GLib.get_user_data_dir();
@@ -69,182 +60,6 @@ function installPlugin() {
 }
 
 
-/**
- * The quick settings menu for Valent.
- */
-const ServiceMenuToggle = GObject.registerClass({
-    GTypeName: 'ValentServiceMenuToggle',
-    Properties: {
-        'service': GObject.ParamSpec.object(
-            'service',
-            'Service',
-            'The remote service',
-            GObject.ParamFlags.READWRITE,
-            Remote.Service.$gtype
-        ),
-    },
-}, class ServiceToggle extends QuickSettings.QuickMenuToggle {
-    constructor(params = {}) {
-        super({
-            label: _('Valent'),
-            icon_name: 'ca.andyholmes.Valent-symbolic',
-            toggle_mode: true,
-            ...params,
-        });
-
-        this._items = new Map();
-
-        this.menu.setHeader('ca.andyholmes.Valent-symbolic',
-            _('Device Connections'));
-
-        this._itemsSection = new PopupMenu.PopupMenuSection();
-        this.menu.addMenuItem(this._itemsSection);
-
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        // TRANSLATORS: A menu option to open the service settings
-        const settingsItem = this.menu.addAction(_('Valent Settings'),
-            this._onSettingsActivated.bind(this));
-        settingsItem.visible = Main.sessionMode.allowSettings;
-        this.menu._settingsActions['ca.andyholme.Valent.desktop'] = settingsItem;
-
-        this.service.bind_property('active', this, 'checked',
-            GObject.BindingFlags.SYNC_CREATE);
-        this._deviceAddedId = this.service.connect('device-added',
-            this._onDeviceAdded.bind(this));
-        this._deviceRemovedId = this.service.connect('device-removed',
-            this._onDeviceRemoved.bind(this));
-
-        this.connect('destroy', this._onDestroy);
-    }
-
-    vfunc_clicked(_clickedButton) {
-        console.debug('toggling service');
-
-        if (this.service.active)
-            this.service.activate_action('quit');
-        else
-            this.service.activate();
-    }
-
-    _onDestroy(actor) {
-        if (actor._deviceAddedId)
-            actor.service.disconnect(actor._deviceAddedId);
-
-        if (actor._deviceRemovedId)
-            actor.service.disconnect(actor._deviceRemovedId);
-    }
-
-    _onDeviceAdded(service_, device) {
-        const item = new Device.MenuItem(device);
-
-        this._items.set(device, item);
-        this._itemsSection.addMenuItem(item);
-
-        this._sync();
-    }
-
-    _onDeviceRemoved(service_, device) {
-        const item = this._items.get(device);
-
-        if (item) {
-            this._items.delete(device);
-            item.destroy();
-        }
-
-        this._sync();
-    }
-
-    _onSettingsActivated(_event) {
-        this.service.activate_action('preferences');
-
-        Main.overview.hide();
-        Main.panel.closeQuickSettings();
-    }
-
-    _sync() {
-        const available = this.service.devices.filter(device => {
-            return (device.state & Remote.DeviceState.CONNECTED) !== 0 &&
-                   (device.state & Remote.DeviceState.PAIRED) !== 0;
-        });
-        const nAvailableDevices = available.length;
-
-        if (nAvailableDevices === 1) {
-            this.label = available[0].name;
-        } else if (nAvailableDevices > 0) {
-            // TRANSLATORS: %d is the number of devices connected
-            this.label = ngettext('%d Connected', '%d Connected',
-                available.length).format(available.length);
-        } else {
-            // TRANSLATORS: The quick settings item label
-            this.label = _('Valent');
-        }
-    }
-});
-
-
-/**
- * A System Indicator that's visible in the panel when devices are connected,
- * with menu items to start or stop the service and open the settings.
- */
-const ServiceIndicator = GObject.registerClass({
-    GTypeName: 'ValentServiceIndicator',
-}, class ServiceIndicator extends QuickSettings.SystemIndicator {
-    constructor() {
-        super();
-
-        this.connect('destroy', this._onDestroy);
-
-        // Service Proxy
-        this.service = new Remote.Service();
-        this._deviceAddedId = this.service.connect('device-added',
-            this._onDeviceAdded.bind(this));
-        this._deviceRemovedId = this.service.connect('device-removed',
-            this._sync.bind(this));
-
-        // Service Indicator
-        this._indicator = this._addIndicator();
-        this._indicator.icon_name = 'ca.andyholmes.Valent-symbolic';
-        this._indicator.visible = false;
-        QuickSettingsMenu._indicators.insert_child_at_index(this, 0);
-
-        // Service Toggle
-        const menuToggle = new ServiceMenuToggle({
-            service: this.service,
-        });
-        this.quickSettingsItems.push(menuToggle);
-        QuickSettingsMenu._addItems(this.quickSettingsItems);
-
-        // Prime the service
-        this.service.sync();
-    }
-
-    _onDestroy(actor) {
-        if (actor.service) {
-            actor.service.disconnect(actor._deviceAddedId);
-            actor.service.disconnect(actor._deviceRemovedId);
-            actor.service.destroy();
-        }
-
-        actor.quickSettingsItems.forEach(item => item.destroy());
-    }
-
-    _onDeviceAdded(service_, device) {
-        device.connect('notify::state', this._sync.bind(this));
-        this._sync();
-    }
-
-    _sync() {
-        const available = this.service.devices.filter(device => {
-            return (device.state & Remote.DeviceState.CONNECTED) !== 0 &&
-                   (device.state & Remote.DeviceState.PAIRED) !== 0;
-        });
-
-        this._indicator.visible = available.length > 0;
-    }
-});
-
-
 let serviceIndicator = null;
 let clipboardInterface = null;
 
@@ -263,7 +78,7 @@ function enable() {
     Notification.patchNotificationSources();
 
     if (serviceIndicator === null)
-        serviceIndicator = new ServiceIndicator();
+        serviceIndicator = new Status.Indicator();
 
     if (clipboardInterface === null)
         clipboardInterface = new Clipboard.Clipboard();
@@ -274,14 +89,10 @@ function enable() {
 function disable() {
     Notification.unpatchNotificationSources();
 
-    if (serviceIndicator !== null) {
-        serviceIndicator.destroy();
-        serviceIndicator = null;
-    }
+    serviceIndicator?.destroy();
+    serviceIndicator = null;
 
-    if (clipboardInterface !== null) {
-        clipboardInterface.destroy();
-        clipboardInterface = null;
-    }
+    clipboardInterface?.destroy();
+    clipboardInterface = null;
 }
 
---
+++ src/extension/status.js
@@ -0,0 +1,419 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2022 Andy Holmes <andrew.g.r.holmes@gmail.com>
+
+/* exported Indicator */
+
+const { Clutter, Gio, GObject, St } = imports.gi;
+
+const Main = imports.ui.main;
+const PopupMenu = imports.ui.popupMenu;
+const QuickSettings = imports.ui.quickSettings;
+const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Extension = ExtensionUtils.getCurrentExtension();
+
+const Remote = Extension.imports.remote;
+
+const _ = ExtensionUtils.gettext;
+const ngettext = ExtensionUtils.ngettext;
+
+
+/**
+ * Get an battery icon name, with high granularity.
+ *
+ * @param {number} percentage - an integer between -1.0 and 100.0
+ * @param {boolean} charging - whether the battery is charging
+ * @returns {string} a themed icon name
+ */
+function _getBatteryIcon(percentage, charging) {
+    // This is particular to KDE Connect
+    if (percentage < 0)
+        return 'battery-missing-symbolic';
+
+    if (percentage >= 100)
+        return 'battery-level-100-charged-symbolic';
+
+    const level = Math.floor(percentage / 10) * 10;
+
+    return charging
+        ? `battery-level-${level}-charging-symbolic`
+        : `battery-level-${level}-symbolic`;
+}
+
+
+/**
+ * A battery widget with an icon and text percentage.
+ */
+const DeviceBattery = GObject.registerClass({
+    GTypeName: 'ValentDeviceBattery',
+    Properties: {
+        'device': GObject.ParamSpec.object(
+            'device',
+            'Device',
+            'The remote device',
+            GObject.ParamFlags.READWRITE,
+            Remote.Device
+        ),
+    },
+}, class DeviceBattery extends St.BoxLayout {
+    constructor(params = {}) {
+        super({
+            style_class: 'valent-device-battery',
+            ...params,
+        });
+
+        // Percentage Label
+        this._label = new St.Label({
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+        this.add_child(this._label);
+        this.label_actor = this._label;
+
+        // Battery Icon
+        this._icon = new St.Icon({
+            fallback_icon_name: 'battery-missing-symbolic',
+            icon_size: 16,
+        });
+        this.add_child(this._icon);
+
+        this.connect('destroy', this._onDestroy.bind(this));
+    }
+
+    get device() {
+        if (this._device === undefined)
+            this._device = null;
+
+        return this._device;
+    }
+
+    set device(device) {
+        if (this.device === device)
+            return;
+
+        this._connectDevice(device);
+
+        this._device = device;
+        this.notify('device');
+    }
+
+    _connectDevice(device = null) {
+        if (this.device !== null) {
+            for (const handlerId of this._actionHandlerIds)
+                this.device.action_group.disconnect(handlerId);
+            this._actionHandlerIds = [];
+        }
+
+        if (device) {
+            this._actionHandlerIds = [
+                device.action_group.connect('action-added::battery.state',
+                    this._onActionChanged.bind(this)),
+                device.action_group.connect('action-removed::battery.state',
+                    this._onActionChanged.bind(this)),
+                device.action_group.connect('action-enabled-changed::battery.state',
+                    this._onActionEnabledChanged.bind(this)),
+                device.action_group.connect('action-state-changed::battery.state',
+                    this._onActionStateChanged.bind(this)),
+            ];
+        }
+
+        this._onActionChanged(device?.action_group, 'battery.state');
+    }
+
+    _onActionChanged(group, name) {
+        if (group?.get_action_enabled(name)) {
+            const value = group.get_action_state(name);
+            this._onActionStateChanged(group, name, value);
+        } else {
+            this.visible = false;
+        }
+    }
+
+    _onActionEnabledChanged(group, name, enabled) {
+        this.visible = enabled;
+
+        if (this.visible)
+            this._onActionChanged(group, name);
+    }
+
+    _onActionStateChanged(_group, name, value) {
+        const {
+            charging = false,
+            percentage = 0.0,
+            'icon-name': iconFallback = 'battery-missing-symbolic',
+            'is-present': isPresent = false,
+        } = value.recursiveUnpack();
+
+        // Disable GThemedIcon's default fallbacks
+        this._icon.gicon = new Gio.ThemedIcon({
+            name: _getBatteryIcon(percentage, charging),
+            use_default_fallbacks: false,
+        });
+        this._icon.fallback_icon_name = iconFallback;
+
+        this._label.text = isPresent ? `${Math.floor(percentage)}%` : '';
+        this.visible = isPresent;
+    }
+
+    _onDestroy(_actor) {
+        if (this.device === null)
+            return;
+
+        for (const handlerId of this._actionHandlerIds)
+            this.device.action_group.disconnect(handlerId);
+    }
+});
+
+
+/**
+ * A menu item for devices.
+ */
+const DeviceMenuItem = GObject.registerClass({
+    GTypeName: 'ValentDeviceMenuItem',
+    Properties: {
+        'device': GObject.ParamSpec.object(
+            'device',
+            'Device',
+            'The remote device',
+            GObject.ParamFlags.READWRITE,
+            Remote.Device
+        ),
+    },
+}, class DeviceMenuItem extends PopupMenu.PopupBaseMenuItem {
+    constructor(device) {
+        super();
+
+        // Workaround parameter parsing
+        this.device = device;
+        this._activatable = false;
+
+        this._icon = new St.Icon({
+            fallback_icon_name: 'computer-symbolic',
+            style_class: 'popup-menu-icon',
+        });
+        this.add_child(this._icon);
+
+        this._label = new St.Label({
+            x_expand: true,
+            y_expand: true,
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+        this.add_child(this._label);
+        this.label_actor = this._label;
+
+        this._battery = new DeviceBattery({ visible: false });
+        this.add_child(this._battery);
+
+        this.bind_property('device', this._battery, 'device',
+            GObject.BindingFlags.SYNC_CREATE);
+        this.device.bind_property('icon-name', this._icon, 'icon-name',
+            GObject.BindingFlags.SYNC_CREATE);
+        this.device.bind_property('name', this._label, 'text',
+            GObject.BindingFlags.SYNC_CREATE);
+        this._stateChangedId = this.device.connect('notify::state',
+            this._sync.bind(this));
+
+        this._sync(this.device);
+        this.connect('destroy', this._onDestroy.bind(this));
+    }
+
+    _onDestroy(_actor) {
+        this.device.disconnect(this._stateChangedId);
+    }
+
+    _sync(device, _pspec) {
+        this.visible = (device.state & Remote.DeviceState.CONNECTED) !== 0 &&
+                       (device.state & Remote.DeviceState.PAIRED) !== 0;
+    }
+});
+
+
+/**
+ * The quick settings menu for Valent.
+ */
+const MenuToggle = GObject.registerClass({
+    GTypeName: 'ValentMenuToggle',
+    Properties: {
+        'service': GObject.ParamSpec.object(
+            'service',
+            'Service',
+            'The remote service',
+            GObject.ParamFlags.READWRITE,
+            Remote.Service.$gtype
+        ),
+    },
+}, class MenuToggle extends QuickSettings.QuickMenuToggle {
+    constructor(params = {}) {
+        super({
+            label: _('Valent'),
+            toggle_mode: true,
+            ...params,
+        });
+
+        this._activeIcon = Gio.Icon.new_for_string(
+            `file://${Extension.path}/data/phonelink-symbolic.svg`);
+        this._inactiveIcon = Gio.Icon.new_for_string(
+            `file://${Extension.path}/data/phonelink-off-symbolic.svg`);
+
+        this.menu.setHeader('ca.andyholmes.Valent-symbolic',
+            _('Device Connections'));
+
+        this._devices = new WeakMap();
+        this._devicesSection = new PopupMenu.PopupMenuSection();
+        this.menu.addMenuItem(this._devicesSection);
+
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        // TRANSLATORS: A menu option to open the service settings
+        const settingsItem = this.menu.addAction(_('Valent Settings'),
+            this._onSettingsActivated.bind(this));
+        settingsItem.visible = Main.sessionMode.allowSettings;
+        this.menu._settingsActions['ca.andyholme.Valent.desktop'] = settingsItem;
+
+        this._activeChangedId = this.service.connect('notify::active',
+            this._sync.bind(this));
+        this._deviceAddedId = this.service.connect('device-added',
+            this._onDeviceAdded.bind(this));
+        this._deviceRemovedId = this.service.connect('device-removed',
+            this._onDeviceRemoved.bind(this));
+
+        this._sync();
+        this.connect('destroy', this._onDestroy.bind(this));
+    }
+
+    vfunc_clicked(_clickedButton) {
+        if (this.service.active)
+            this.service.activate_action('quit');
+        else
+            this.service.activate();
+    }
+
+    _onDestroy(_actor) {
+        this.service.disconnect(this._activeChangedId);
+        this.service.disconnect(this._deviceAddedId);
+        this.service.disconnect(this._deviceRemovedId);
+    }
+
+    _onDeviceAdded(_service, device) {
+        const menuItem = new DeviceMenuItem(device);
+        this._devicesSection.addMenuItem(menuItem);
+
+        const stateChangedId = device.connect('notify::state',
+            this._sync.bind(this));
+
+        this._devices.set(device, [menuItem, stateChangedId]);
+        this._sync();
+    }
+
+    _onDeviceRemoved(_service, device) {
+        const [menuItem, stateChangedId] = this._devices.get(device) ?? [];
+
+        if (menuItem)
+            menuItem.destroy();
+
+        if (stateChangedId)
+            device.disconnect(stateChangedId);
+
+        this._devices.delete(device);
+        this._sync();
+    }
+
+    _onSettingsActivated(_event) {
+        this.service.activate_action('preferences');
+
+        Main.overview.hide();
+        Main.panel.closeQuickSettings();
+    }
+
+    _sync() {
+        const available = this.service.devices.filter(device => {
+            return (device.state & Remote.DeviceState.CONNECTED) !== 0 &&
+                   (device.state & Remote.DeviceState.PAIRED) !== 0;
+        });
+
+        if (available.length === 1) {
+            this.label = available[0].name;
+        } else if (available.length > 0) {
+            // TRANSLATORS: %d is the number of devices connected
+            this.label = ngettext('%d Connected', '%d Connected',
+                available.length).format(available.length);
+        } else {
+            // TRANSLATORS: The quick settings item label
+            this.label = _('Valent');
+        }
+
+        this.checked = this.service.active;
+        this.gicon = this.service.active
+            ? this._activeIcon
+            : this._inactiveIcon;
+    }
+});
+
+
+/**
+ * The service indicator for Valent.
+ */
+var Indicator = GObject.registerClass({
+    GTypeName: 'ValentIndicator',
+}, class Indicator extends QuickSettings.SystemIndicator {
+    constructor() {
+        super();
+
+        // Service Proxy
+        this._devices = new WeakMap();
+        this._service = new Remote.Service();
+        this._service.connect('device-added',
+            this._onDeviceAdded.bind(this));
+        this._service.connect('device-removed',
+            this._onDeviceRemoved.bind(this));
+
+        // Indicator Icon
+        this._icon = this._addIndicator();
+        this._icon.icon_name = 'ca.andyholmes.Valent-symbolic';
+        this._icon.visible = false;
+
+        // Service Toggle
+        const menuToggle = new MenuToggle({ service: this._service });
+        this.quickSettingsItems.push(menuToggle);
+
+        QuickSettingsMenu._addItems(this.quickSettingsItems);
+        QuickSettingsMenu._indicators.insert_child_at_index(this, 0);
+        this.connect('destroy', this._onDestroy.bind(this));
+
+        // Prime the service
+        this._service.sync();
+    }
+
+    _onDestroy(_actor) {
+        this._service.destroy();
+        this.quickSettingsItems.forEach(item => item.destroy());
+    }
+
+    _onDeviceAdded(_service, device) {
+        const stateChangedId = device.connect('notify::state',
+            this._sync.bind(this));
+
+        this._devices.set(device, stateChangedId);
+        this._sync();
+    }
+
+    _onDeviceRemoved(_service, device) {
+        const stateChangedId = this._devices.get(device);
+
+        if (stateChangedId)
+            device.disconnect(stateChangedId);
+
+        this._devices.delete(device);
+        this._sync();
+    }
+
+    _sync() {
+        const available = this._service.devices.filter(device => {
+            return (device.state & Remote.DeviceState.CONNECTED) !== 0 &&
+                   (device.state & Remote.DeviceState.PAIRED) !== 0;
+        });
+
+        this._icon.visible = available.length > 0;
+    }
+});
+
