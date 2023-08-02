fd3c1c016cd2d11b5fe36db8ceba2bbc98568734
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-08-02 16:44:50 -0700

    refactor: port extension.js to class pattern

--- src/extension.js
+++ src/extension.js
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: Andy Holmes <andrew.g.r.holmes@gmail.com>
 
-/* exported init, enable, disable */
+/* exported init */
 
 const { Gio } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
-const Extension = ExtensionUtils.getCurrentExtension();
+const ExtensionMeta = ExtensionUtils.getCurrentExtension();
 
-const Notification = Extension.imports.notification;
-const Session = Extension.imports.session;
-const Status = Extension.imports.status;
+const Notification = ExtensionMeta.imports.notification;
+const Session = ExtensionMeta.imports.session;
+const Status = ExtensionMeta.imports.status;
 
 
 /**
@@ -19,45 +19,47 @@ const Status = Extension.imports.status;
  * @param {string} name - An icon name
  * @returns {Gio.Icon} a `Gio.Icon`
  */
-Extension.getIcon = function (name) {
+ExtensionMeta.getIcon = function (name) {
     return Gio.Icon.new_for_string(
         `file://${this.path}/icons/valent-${name}-symbolic.svg`);
 };
 
 
-let serviceIndicator = null;
+class Extension {
+    constructor() {
+        this._indicator = null;
+    }
+
+    enable() {
+        Notification.patchNotificationSources();
+        Session.enable();
+
+        this._indicator = new Status.Indicator();
+    }
+
+    /**
+     * Disable the extension.
+     *
+     * The extension will be re-enabled in the `unlock-dialog` session mode so that
+     * quick settings behave like other services, and modifications to components
+     * like notifications will remain while Valent runs in the background.
+     *
+     * See: https://gjs.guide/extensions/review-guidelines/review-guidelines#session-modes
+     */
+    disable() {
+        Notification.unpatchNotificationSources();
+        Session.disable();
+
+        this._indicator?.destroy();
+        this._indicator = null;
+    }
+}
 
 
 /** */
-function init() {
+function init(meta) {
     ExtensionUtils.initTranslations();
-}
 
-
-/** */
-function enable() {
-    Notification.patchNotificationSources();
-    Session.enable();
-
-    if (serviceIndicator === null)
-        serviceIndicator = new Status.Indicator();
-}
-
-
-/**
- * Disable the extension.
- *
- * The extension will be re-enabled in the `unlock-dialog` session mode so that
- * quick settings behave like other services, and modifications to components
- * like notifications will remain while Valent runs in the background.
- *
- * See: https://gjs.guide/extensions/review-guidelines/review-guidelines#session-modes
- */
-function disable() {
-    Notification.unpatchNotificationSources();
-    Session.disable();
-
-    serviceIndicator?.destroy();
-    serviceIndicator = null;
+    return new Extension();
 }
 
