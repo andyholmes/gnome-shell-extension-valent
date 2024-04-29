92be979f351d6c1c25e784a3893b1e30c671e784
Andy Holmes <andrew.g.r.holmes@gmail.com>
2024-04-29 02:58:47 -0700

    refactor(extension): pass an InjectionManager to components
    Use a extension-wide `InjectionManager` for class instance and
    prototype modifications, passing it through to components so they
    can override and restore methods.

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: Andy Holmes <andrew.g.r.holmes@gmail.com>
 
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import {Extension, InjectionManager} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 import * as Notification from './notification.js';
 import * as Status from './status.js';
@@ -9,8 +9,9 @@ import * as Status from './status.js';
 
 export default class ValentExtension extends Extension {
     enable() {
-        Notification.enable();
+        this._injectionManager = new InjectionManager();
 
+        Notification.enable(this._injectionManager);
         this._indicator = new Status.Indicator();
     }
 
@@ -24,10 +25,12 @@ export default class ValentExtension extends Extension {
      * See: https://gjs.guide/extensions/review-guidelines/review-guidelines#session-modes
      */
     disable() {
-        Notification.disable();
-
         this._indicator?.destroy();
         this._indicator = null;
+        Notification.disable(this._injectionManager);
+
+        this._injectionManager.clear();
+        this._injectionManager = null;
     }
 }
 
